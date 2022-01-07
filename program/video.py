1e7a236e288d8eaeb44a497f04dc82cc581b1383
Rishabhbhan5 <90978799+Rishabhbhan5@users.noreply.github.com>
2022-01-07 08:59:13 +0530

    Update video.py

--- program/video.py
+++ program/video.py
@@ -63,7 +63,7 @@ async def vplay(c: Client, m: Message):
         [
             [
                 InlineKeyboardButton(text="• Mᴇɴᴜ", callback_data="cbmenu"),
-                InlineKeyboardButton(text="• Group", url=f"https://t.me/lisasupportchat"),
+                InlineKeyboardButton(text="• Group", url=f"https://t.me/{GROUP_SUPPORT}"),
             ]
         ]
     )
