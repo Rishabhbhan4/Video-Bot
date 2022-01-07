55bb37742e612eced83c9b32354dc00776175776
Rishabhbhan5 <90978799+Rishabhbhan5@users.noreply.github.com>
2022-01-07 08:46:09 +0530

    Update video.py

--- program/video.py
+++ program/video.py
@@ -8,7 +8,7 @@ import asyncio
 from config import ASSISTANT_NAME, BOT_USERNAME, IMG_1, IMG_2
 from driver.filters import command, other_filters
 from driver.queues import QUEUE, add_to_queue
-from driver.veez import call_py, user
+from driver.jennie import call_py, user
 from pyrogram import Client
 from pyrogram.errors import UserAlreadyParticipant, UserNotParticipant
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
