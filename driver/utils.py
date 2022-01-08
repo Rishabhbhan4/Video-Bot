8b3b9b40f475b7e4b3c3a71c368cce92d44044ba
Rishabhbhan5 <90978799+Rishabhbhan5@users.noreply.github.com>
2022-01-08 20:47:26 +0530

    Update utils.py

--- driver/utils.py
+++ driver/utils.py
@@ -1,6 +1,6 @@
 import os
 import asyncio
-from driver.veez import bot, call_py
+from driver.jennie import bot, call_py
 from pytgcalls.types import Update
 from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
 from driver.queues import QUEUE, clear_queue, get_queue, pop_an_item
