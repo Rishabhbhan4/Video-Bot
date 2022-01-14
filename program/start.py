ce8a9981c41251d99df276a2b9419c93929fc8f2
Rishabhbhan5 <90978799+Rishabhbhan5@users.noreply.github.com>
2022-01-14 20:16:48 +0530

    Update start.py

--- program/start.py
+++ program/start.py
@@ -56,9 +56,9 @@ async def start_(client: Client, message: Message):
         f"""✨ **Welcome {message.from_user.mention()} !**\n
 💭 [{BOT_NAME}](https://t.me/{BOT_USERNAME}) **Allows you to play music and video on groups through the new Telegram's video chats!**
 
-/help **Find out all the Bot's commands and how they work by clicking on the » 📚 Commands button!**
+❂ **Find out all the Bot's commands and how they work by clicking on the » 📚 Commands button!**
 
-🔖 **To know how to use this bot, please click on the » ❓ Basic Guide button!**
+❂ **To know how to use this bot, please click on the » ❓ Basic Guide button!**
 """,
         reply_markup=InlineKeyboardMarkup(
             [
